ab7d14557af82b5ec365dfe893d07dce62a36802
Meda Ramakrishna <medar@infinite.com>
2022-10-10 08:11:49 +0530

    Assignment_5

---
+++ CSHARP/Assignment5/Assignment_5/Assignment_5/Occurences.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Assignment_5
+{
+    class Occurences
+    {
+        public static void Main()
+        {
+
+            string str = "RAMAKRISHNA";
+            Console.WriteLine("The number of Occurences of a String #####  " + str + "  ######## is :");
+
+            char ch = 'A';
+
+            int cont = 0;
+            foreach (char c in str)
+            {
+                if (c == ch)
+                {
+                    cont++;
+                }
+            }
+
+            Console.WriteLine(cont);
+            Console.ReadLine();
+        }
+    }
+}
